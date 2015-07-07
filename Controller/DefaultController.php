c80784650c44e0a2f07210a105d9378d00ed2882
Febin John James <febinjohnjames@Febins-MacBook-Pro.local>
2015-07-07 19:08:21 +0530

    Invoices Added

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -96,4 +96,73 @@ class DefaultController extends ContainerAware
         return new Response($subscriptions, 200);
     }
 
+    public function invoicesAction(Request $request)
+    {
+        try {
+            $customer_id = $request->query->get('customer_id');
+            $limit = $request->query->get('limit') ? $request->query->get('limit') : 10;
+            $starting_after = $request->query->get('starting_after') ? $request->query->get('starting_after') : -1;
+            $ending_before = $request->query->get('ending_before') ? $request->query->get('ending_before') : -1;
+            $date = $request->query->get('date') ? $request->query->get('date') : -1;
+
+            $request_array = array("customer"=>$customer_id, "limit"=>$limit);
+            if ($starting_after != -1) {
+                $request_array["starting_after"] = $starting_after;
+            }
+            if ($ending_before != -1) {
+                $request_array["ending_before"] = $ending_before;
+            }
+            if ($date != -1) {
+                $date_params = array("gt"=>$date);
+                $request_array["date"] = $date_params;
+            }
+
+            \Stripe\Stripe::setApiKey("sk_test_DxL4C2nUSAVBILG7FtnY4mYs");
+            $response = \Stripe\Invoice::all($request_array);
+            $invoices = array();
+            $i = 0;
+            foreach ($response['data'] as $invoice) {
+                $invoice_item = array(
+                    "date" => $invoice['date'],
+                    "end" => $invoice['lines']['data'][0]['period']['end'],
+                    "name" => $invoice['lines']['data'][0]['plan']['name'],
+                    "id" => $invoice['id'],
+                    "interval" => $invoice['lines']['data'][0]['plan']['interval'],
+                    "total" => $invoice['total']
+                );
+                $invoices[$i] = $invoice_item;
+                $i++;
+            }
+            $invoices = json_encode($invoices);
+
+        } catch (\Stripe\Error\ApiConnection $e) {
+            // Network communication with Stripe failed
+            $error = array("error" => "Connection Error");
+
+            return new Response(json_encode($error), 422);
+        } catch (\Stripe\Error\Authentication $e) {
+            // Authentication with Stripe's API failed
+            $error = array("error" => "Invalid API Key");
+
+            return new Response(json_encode($error), 422);
+        } catch (\Stripe\Error\InvalidRequest $e) {
+            // Invalid parameters were supplied to Stripe's API
+            $error = array("error" => "Invalid Request","array"=> $request_array);
+
+            return new Response(json_encode($error), 422);
+        } catch (\Stripe\Error\Base $e) {
+            // Display a very generic error to the user, and maybe send
+            $error = array("error" => $e);
+
+            return new Response(json_encode($error), 422);
+        } catch (Exception $e) {
+            // Something else happened, completely unrelated to Stripe
+            $error = array("error" => $e);
+
+            return new Response(json_encode($error), 422);
+        }
+
+        return new Response($invoices, 200);
+    }
+
 }
